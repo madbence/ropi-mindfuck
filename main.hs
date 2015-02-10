6a4e96d467dd9588be2c66d20511d900c52d9b0d
Bence Dányi <bence@danyi.me>
2015-02-11 00:31:33 +0100

    :rage4: :rage4: :rage4: :rage4:

--- main.hs
+++ main.hs
@@ -1,45 +1,113 @@
+import Data.List
+
 type Node = Int
 type Edge = (Node, Node)
 type Edges = [Edge]
+type Pairing = Edges
+type Nodes = [Node]
+type Path = Edges
+type Paths = [Path]
+type Graph = ((Nodes, Nodes), Edges)
 
-flatten :: [[a]] -> [a]
-flatten [[]] = []
-flatten (x:xs) = x ++ flatten xs
+-- max pairing with initial pairing
+maxPairing :: Graph -> Pairing -> Pairing
+maxPairing g p =
+  case augmentingPath g p of
 
-paired :: Edges -> Node -> Bool
-paired [] n = False
-paired ((u, v):ps) w
-  | u == w    = True
-  | v == w    = True
-  | otherwise = paired ps w
+    -- if there is no augmenting path, the pairing is maximal
+    Nothing   -> p
 
-edges :: Edges -> Node -> Edges
-edges [] _ = []
-edges (e@(u, v):es) w
-  | u == w    = (e:edges es w)
-  | v == w    = (e:edges es w)
-  | otherwise = edges es w
+    -- if there is an augmenting path, construct new pairing
+    Just path -> let
 
-nodes :: Edges -> [Node]
-nodes 
+      -- every odd edge is part of the new pairing
+      replacement = concatMap
+        (\(edge, index) -> if index `mod` 2 == 1 then [edge] else [])
+        (zip path [1..length path])
 
+      -- construct new pairing
+      improvedPairing = (p `clean` path) ++ replacement
 
-isInEdgeList :: Edges -> Edge -> Bool
-isInEdgeList [] _ = False
-isInEdgeList ((u, v):es) e@(u', v')
-  | u == u' && v == v' = True
-  | otherwise          = isInEdgeList es e
+      in maxPairing g improvedPairing
+  where
 
-isAugmenting :: Edges -> Edges -> Bool
-isAugmenting ps path = all isRight (zip path [1..(length path)])
-  where isRight (e, n) = case n `mod` 2 of 0 -> isInEdgeList ps e
-                                           1 -> not (isInEdgeList ps e)
+    -- edge-list difference
+    clean p p' = filter (\(u, v) -> (u, v) `notElem` p' && (v, u) `notElem` p') p
 
-augmentingPath :: Edges -> Edges -> Maybe Edges
-augmentingPath es ps = augmentingPath' es ps (nodes ps)
+-- find augmenting path, if possible
+augmentingPath :: Graph -> Pairing -> Maybe Path
+augmentingPath g p = let
+  ((topNodes, bottomNodes), edges) = g
 
-augmentingPath' es ps [] = Nothing
-augmentingPath' es ps (n:ns) = case augmentingPath'' es ps n [] of Nothing -> augmentingPath' es ps ns
-                                                                   Just path -> path
+  -- select disconnected nodes
+  from = filter (disconnected p) bottomNodes
+  to   = filter (disconnected p) topNodes
 
--- TODO: augmentingPath'''
+  -- search for alternating paths from bottom nodes to top nodes
+  paths = concatMap (alternatingPaths g p) [(u, v) | u <- from, v <- to]
+  in case paths of
+
+    -- If there are no good alternating paths, there is no augmenting path
+    []       -> Nothing
+
+    -- the first found path is just fine
+    (path:_) -> Just path
+  where
+    disconnected es w = all (\(u, v) -> u /= w && v /= w) es
+
+-- alternating paths
+alternatingPaths :: Graph -> Pairing -> (Node, Node) -> Paths
+alternatingPaths g p (from, to) = ap from [from]
+  where
+
+    -- dfs
+    ap current visited = let
+
+      -- get neighbour nodes
+      ns = neighbours current
+
+      in
+        -- if target is neighbour
+        if to `elem` ns
+
+          -- we're done
+          then makePath (to:visited)
+
+          -- if target is not a neighbour, then search recursively from neighbours
+          else let
+
+            -- map every candidate to Path-list
+            f = \n -> ap n (n:visited)
+
+            -- select candidate nodes based on the current parity
+            candidates = let
+
+              f = \n -> let
+
+                -- neighbour is marked, if connecting edge is part of the current pairing
+                marked = (n, current) `elem` p || (current, n) `elem` p
+
+                -- parity is calculated from path length
+                parity = length visited `mod` 2 == 1
+
+                -- node is candidate if we are at the bottom half, and node is not marked
+                -- or if we are at the top half and node is marked
+                in parity == True && not marked || parity == False && marked
+
+              -- select candidates
+              in filter f ns
+
+            -- concat path-list from candidates
+            in concatMap f candidates
+
+    -- restore Edges from Nodes
+    makePath :: Nodes -> [Edges]
+    makePath ns = [zip ns (drop 1 ns)]
+
+    -- neighbour nodes
+    neighbours :: Node -> Nodes
+    neighbours n = let
+      ((top, bottom), edges) = g
+      in if n `elem` bottom
+        then filter (\v -> (v, n) `elem` edges) top
+        else filter (\v -> (n, v) `elem` edges) bottom
